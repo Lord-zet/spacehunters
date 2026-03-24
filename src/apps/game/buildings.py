30e029d63e2a9f33669b2be67d823179f9174ad1
Lord Zet <marcin.zajec@wp.pl>
2026-03-24 14:07:55 +0100

    feat: add build time for buildings

--- src/apps/game/buildings.py
+++ src/apps/game/buildings.py
@@ -11,11 +11,13 @@ BUILDINGS = {
     "metal_mine": {
         "level_field": "metal_mine_level",
         "base_cost": {"metal": 100},
+        "build_time": 60,
         "production_fn": metal_mine_production,
     },
     "crystal_mine": {
         "level_field": "crystal_mine_level",
         "base_cost": {"metal": 80},
+        "build_time": 90,
         "production_fn": crystal_mine_production,
     },
 }
